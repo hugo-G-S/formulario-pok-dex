74f254f7bf85d552a2ab537e84dc423fd4cfde4b
hugo-G-S <hugoyeager7@gmail.com>
2025-11-26 21:26:33 -0600

    Update vite.config.js
    actualización Git para vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Corregido: "react" completo
 
 export default defineConfig({
   plugins: [react()],
+  base: '/formulario-pok-dex/', // ¡AGREGA ESTA LÍNEA!
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
